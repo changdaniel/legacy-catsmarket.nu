ea6d0c42d40a80b895b31a98144feda23caa9687
changdaniel116 <changdaniel116@yahoo.com>
2019-05-14 03:15:27 -0500

    Added pages and functionality for email save and display

--- basic_server/server.js
+++ basic_server/server.js
@@ -7,6 +7,9 @@ app.set('view engine', 'html');
 exports.index = function(req, res){
     res.render('index', { title: 'ejs' });};
 
+var selleremail = "";
+var buyeremail = "";
+
 app.get('/', function (request, response) {
 
   response.render('home')
@@ -22,6 +25,20 @@ app.get('/buy-search-resolved', function (request, response) {
     response.render('buy-search-resolved')
 })
 
+app.get('/order', function (request, response) {
+
+  response.render('order')
+})
+
+app.get('/buyer-email-processing', function (request, response) {
+
+  buyeremail = request.query.email
+  response.render('order-confirmed', 
+  {buyeremail:buyeremail,
+  selleremail:selleremail})
+})
+
+
 app.get('/sell-search', function (request, response) {
 
     response.render('sell-search')
@@ -32,13 +49,22 @@ app.get('/sell-search-resolved', function (request, response) {
     response.render('sell-search-resolved')
 })
 
-app.get('/list-1', function (request, response) {
+app.get('/list', function (request, response) {
 
-    response.render('list-1')
+    response.render('list')
+})
+
+app.get('/seller-email-processing', function (request, response) {
+
+  selleremail = request.query.email;
+  console.log(selleremail)
+  response.render('listing-confirmed', {selleremail:selleremail});
+
 })
 
 
 app.listen(8080, function () {
+
   console.log('Server listening on port 8080');
 
 });
